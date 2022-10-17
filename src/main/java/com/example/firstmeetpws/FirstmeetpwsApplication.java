26ecaa7fac2ee4759233ef215609be0518c0910f
dhiastii <dhiastiputri.jayanti@gmail.com>
2022-10-17 22:26:24 +0700

    First Commit

---
+++ src/main/java/com/example/firstmeetpws/FirstmeetpwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstmeetpws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstmeetpwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstmeetpwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstmeetpws/FirstmeetpwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstmeetpws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstmeetpwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
